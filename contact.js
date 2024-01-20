86da71377c37e95a26ad864b29e8ba5961bfa7f9
shhh5675 <151127407+shhh5675@users.noreply.github.com>
2024-01-20 17:42:54 +0530

    Add files via upload
    there is basic project for React.

---
+++ contact.js
@@ -0,0 +1,29 @@
+const contact=[
+    {
+    name:"Beyonce" ,
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2O9I5zh7V-BOb2eHeTU6rN6uviSto2Bv1XQ&usqp=CAU",
+    tel:"+123 4567 89",
+    email:"beyounce@gmail.com"
+
+    },
+    {
+    name:"Jack Baurer",
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTn2USTWblPnnwGtI8gVOYLUQ71mc_iufTLA&usqp=CAU",
+    tel:"+234 56 789",
+    email:"Jack@gmail.com"
+
+    },
+    {
+    name:"chunk Norris",
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvP5DPuaURriwkDivGQLeYqT89hWH7P4OogA&usqp=CAU",
+    tel:"+98 765 432",
+    email:"chunk@gmail.com"
+        
+
+
+    }
+]
+
+
+export default contact;
+
